68cdf2a89689df688864220607695bfc20c941cd
ashutosh <ashutosh@example.com>
2025-06-09 03:53:58 +0530

    my commit

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 // console.log('Resolved API_BASE_URL:', API_BASE_URL);
 
 //const API_BASE_URL =  'http://localhost:8080/api';
-const API_BASE_URL = 'https://realbackend-iybp.onrender.com/api';
+const API_BASE_URL = 'https://expertpicksbackend.onrender.com:8080/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
